6f7cc5bf430c9ce4af4e7932dff89945b8309c3d
Dushan Randika <dushanrandika1994@gmail.com>
2019-07-10 15:55:44 +0530

    Minor Changes

--- src/actions/test.js
+++ src/actions/test.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export function getTestData(dropVal1,dropval2,dropval3){
+export function getTestData(dropVal){
   console.log("DROP VAL", dropVal)
     return (dispatch) => {
-        axios.get("http://localhost:3050/"+dae+"?classname="+dropVal)
+        axios.get("http://localhost:3050/Instances?classname="+dropVal)
           .then(res => {
             dispatch({ type: 'GET_TEST', payload: res.data })
           })
